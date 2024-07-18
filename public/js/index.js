45c6dcba2f9fe8f27caa0a166f345f2c309e31ec
jossanCosta <jocostasan@gmail.com>
2024-07-18 14:57:35 -0300

    add cesium

--- public/js/index.js
+++ public/js/index.js
@@ -16,7 +16,17 @@ $(".bar-center-buttons a").click(function () {
     }
 });
 
-$(".bar-center-buttons a").click(function() {
+$(".bar-center-buttons a").click(function () {
     $(".bar-center-buttons a").removeClass('active-button')
     $(this).addClass('active-button')
+
+    switch ($(this).attr('id')) {
+        case '3d-button':
+            $('#map-sig').hide();
+            $('#map-3d-container').show();
+            break;
+        default:
+            $('#map-3d-container').hide();
+            $('#map-sig').show();
+    }
 });
