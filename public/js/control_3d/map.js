35b8becdcdb47298fea7c1a55a610411de01834d
dinizime <diniz.ime@gmail.com>
2024-08-03 08:52:27 -0300

    traduz cesium

--- public/js/control_3d/map.js
+++ public/js/control_3d/map.js
@@ -7,7 +7,7 @@ var { west, south, east, north } = {
 var extent = new Cesium.Rectangle.fromDegrees(west, south, east, north)
 Cesium.Camera.DEFAULT_VIEW_RECTANGLE = extent;
 Cesium.Camera.DEFAULT_VIEW_FACTOR = 0;
-// Cesium.Ion.defaultAccessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiJkMzViMDllOS0zMDNhLTRlNjgtODRmYi1iYzQyNmY3ZmEyYWIiLCJpZCI6MjA1ODQ1LCJpYXQiOjE3MTE5ODI0Mzd9.BhKXygD0YSiMuUVD1o7AfOmWKCOlS3lCMQuQMViPQac';
+
 var map = new Cesium.Viewer("map-3d", {
     infoBox: false,
     shouldAnimate: false,
@@ -16,11 +16,10 @@ var map = new Cesium.Viewer("map-3d", {
     homeButton: false,
     sceneModePicker: false,
     baseLayerPicker: false,
-    navigationHelpButton: false,
+    navigationHelpButton: true,
     animation: false,
     timeline: false,
-    fullscreenButton: false,
-    // globe: false
+    fullscreenButton: false
 });
 map.scene.globe.baseColor=Cesium.Color.BLACK;
 map.scene.skyAtmosphere.show = true;
